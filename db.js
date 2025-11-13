3296a6103e8d2346cac5bde292e29c290f18ceda
Priyotush <priyotushpaul40@gmail.com>
2025-11-13 20:59:56 +0600

    mongodb database connect and all method

---
+++ db.js
@@ -0,0 +1,28 @@
+// db.js
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+
+const uri = process.env.MONGO_URI;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+let db;
+
+async function connectDB() {
+  await client.connect();
+  db = client.db("fineaseDB"); // database name
+  console.log("MongoDB connected!");
+  return db;
+}
+
+function getDB() {
+  if (!db) throw new Error("Database not connected!");
+  return db;
+}
+
+module.exports = { connectDB, getDB };
--- server.js
+++ server.js
@@ -1,45 +1,45 @@
 // server.js
-import cors from "cors";
-import dotenv from "dotenv";
-import express from "express";
-import { MongoClient, ObjectId } from "mongodb";
+const express = require("express");
+const cors = require("cors");
+const { connectDB, getDB } = require("./db");
+const { ObjectId } = require("mongodb");
 
-dotenv.config();
+require("dotenv").config();
+
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-const client = new MongoClient(process.env.MONGO_URI);
-let transactionsCollection;
+const PORT = process.env.PORT || 5000;
 
-client.connect().then(() => {
-  const db = client.db("fineaseDB");
-  transactionsCollection = db.collection("transactions");
-  console.log("MongoDB connected");
-});
+connectDB().then(() => console.log("Database ready!"));
 
 // ---------- Add Transaction ----------
 app.post("/transactions", async (req, res) => {
   const transaction = req.body;
   transaction.date = new Date(transaction.date);
-  const result = await transactionsCollection.insertOne(transaction);
+  const result = await getDB()
+    .collection("transactions")
+    .insertOne(transaction);
   res.send(result);
 });
 
 // ---------- Get All Transactions by User ----------
 app.get("/transactions", async (req, res) => {
   const { email } = req.query;
-  const query = { userEmail: email };
-  const transactions = await transactionsCollection.find(query).toArray();
+  const transactions = await getDB()
+    .collection("transactions")
+    .find({ userEmail: email })
+    .toArray();
   res.send(transactions);
 });
 
 // ---------- Get Single Transaction ----------
 app.get("/transactions/:id", async (req, res) => {
   const { id } = req.params;
-  const transaction = await transactionsCollection.findOne({
-    _id: new ObjectId(id),
-  });
+  const transaction = await getDB()
+    .collection("transactions")
+    .findOne({ _id: new ObjectId(id) });
   res.send(transaction);
 });
 
@@ -48,21 +48,19 @@ app.put("/transactions/:id", async (req, res) => {
   const { id } = req.params;
   const updatedData = req.body;
   updatedData.date = new Date(updatedData.date);
-  const result = await transactionsCollection.updateOne(
-    { _id: new ObjectId(id) },
-    { $set: updatedData }
-  );
+  const result = await getDB()
+    .collection("transactions")
+    .updateOne({ _id: new ObjectId(id) }, { $set: updatedData });
   res.send(result);
 });
 
 // ---------- Delete Transaction ----------
 app.delete("/transactions/:id", async (req, res) => {
   const { id } = req.params;
-  const result = await transactionsCollection.deleteOne({
-    _id: new ObjectId(id),
-  });
+  const result = await getDB()
+    .collection("transactions")
+    .deleteOne({ _id: new ObjectId(id) });
   res.send(result);
 });
 
-const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
